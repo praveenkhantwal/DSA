e1b46c06151a80bbddc3b42e598fcc85f11c577e
praveenkhantwal <45765064+praveenkhantwal@users.noreply.github.com>
2022-04-01 15:34:02 +0530

    Create is_palindrome.py

---
+++ is_palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+	def isPalindrome(self, S):
+	    if S== S[::-1]:
+	        return 1
+	    else:
+	        return 0
+        
+#input code(boilerplate code)  can be found at GFG=geeksforgeeks
